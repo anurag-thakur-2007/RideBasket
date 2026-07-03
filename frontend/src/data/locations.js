21c58eae845670fc92b1b6e15770b44db44cf251
Anurag Thakur <atanuragthakurpro@gmail.com>
2026-07-03 21:05:40 +0530

    Increased pickup and drop-off locations

--- frontend/src/data/locations.js
+++ frontend/src/data/locations.js
@@ -1,16 +1,15 @@
 export const LOCATIONS = [
    "VIT Bhopal Main Gate",
   "Bhopal Junction Railway Station",
-  "Rani Kamlapati Railway Station",
+  "Rani Kamlapati Railway Station", 
+  "Sehore Railway Station",
+  "Sant Hirdaram Nagar Railway Station",
   "Bhopal Airport",
   "Indore Airport",
-
-  // ===== VIT Vellore =====
   "VIT Vellore Main Gate",
   "Katpadi Junction Railway Station",
   "Vellore Bus Stand",
 
-  // ===== VIT Chennai =====
   "VIT Chennai Main Gate",
   "Tambaram Railway Station",
   "Chennai Central Railway Station",
@@ -18,7 +17,6 @@ export const LOCATIONS = [
   "CMBT Bus Terminus",
   "Koyambedu Metro Station",
 
-  // ===== VIT AP =====
   "VIT AP Main Gate",
   "Vijayawada Junction Railway Station",
   "Vijayawada Bus Stand",
